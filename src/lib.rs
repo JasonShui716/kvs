8617eb1bc20627c92b238ea516cb14ebb994a3f7
jiaxin.shui <jiaxin.shui@yitu-inc.com>
2020-01-29 15:00:07 +0800

    add doc:

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
+//!
+//!  A Key-Value in-memory storage.
+//!
+//! Example:
+//!
+//! ```rust
+//! # use kvs::KvStore;
+//! let mut store = KvStore::new();
+//! store.set("key1".to_owned(), "value1".to_owned());
+//! assert_eq!(store.get("key1".to_owned()), Some("value1".to_owned()));
+//! store.remove("key1".to_owned());
+//! assert_eq!(store.get("key1".to_owned()), None);
+//! ```
 use std::collections::HashMap;
 
+/// The struct contains inner data strcture.
 #[derive(Debug, Default)]
 pub struct KvStore {
     inner: HashMap<String, String>,
